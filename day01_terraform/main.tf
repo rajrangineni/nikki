ab16e6e3f66f55b2cfa2c75e2b77a238ba65c8f0
nikhitha <meenigenikhitha@gmail.com>
2024-05-10 10:14:27 +0530

    day01

---
+++ day01_terraform/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "name" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    tags = {
+      Name = "ashok"
+
+    }
+  
+}
+resource "aws_s3_bucket" "name" {
+  bucket = "asdfghjkjhgfl"
+
+  
+}
+
+
+
+
