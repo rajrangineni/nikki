2947e37b2bf77d251a173416c34ecfc8232af693
nikhitha <meenigenikhitha@gmail.com>
2024-05-20 21:27:44 +0530

    s3moduleex

---
+++ day-8-modules/variable.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    description = "will give values"
+    type = string
+    default = ""
+  
+}
+variable "key_name" {
+    type = string
+    default = ""
+  
+}
+variable "instance_type" {
+    type = string
+    default = ""
+
+  }
+
