91401421307de04e1651158773a533334fbe2df0
meenigenikhitha <168175568+meenigenikhitha@users.noreply.github.com>
2024-05-23 16:22:01 +0530

    Delete dynamo_ec2 directory

--- dynamo_ec2/backend.tf
+++
@@ -1,9 +0,0 @@
-#terraform {
- # backend "s3" {
-   # key = "terraform.tfstate"
-   # region = "us-east-2"
-   # dynamodb_table = "aws-dynamo-table-practice"
-    #encrypt = true
-    
-  #}
-#}
--- dynamo_ec2/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "name" {
-    ami = "ami-0cc9838aa7ab1dce7"
-    instance_type = "t2.micro"
-    key_name = "nikki-keypair"
-    tags = {
-      Name = "nikki"
-    }
-  
-}
