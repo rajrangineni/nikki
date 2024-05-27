19821d0340ec759332177e205c08a6d65507fe53
rajrangineni <168529818+rajrangineni@users.noreply.github.com>
2024-05-27 18:23:11 +0530

    Update main.tf

--- day01_terraform/main.tf
+++ day01_terraform/main.tf
@@ -3,13 +3,13 @@ resource "aws_instance" "name" {
     instance_type = var.instance_type
     key_name = var.key_name
     tags = {
-      Name = "raj"
+      Name = "dspraj"
 
     }
   
 }
 resource "aws_s3_bucket" "name" {
-  bucket = "zwxcvgujvbnmkasdfcxcvbn"
+  bucket = "zwxcqwertdsaasdfgbnmkasdfcxcvbn"
 
   
 }
